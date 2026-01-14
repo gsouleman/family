724e5ded2f516f22022d7dc49f4612246e510a63
ghouenzen <gsouleman@gmail.com>
2026-01-14 07:26:38 +0100

    feat: set login page as landing page

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => (
                 <Sonner />
                 <BrowserRouter>
                   <Routes>
-                    <Route path="/" element={<Index />} />
+                    <Route path="/" element={<Login />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/admin" element={<AdminDashboard />} />
